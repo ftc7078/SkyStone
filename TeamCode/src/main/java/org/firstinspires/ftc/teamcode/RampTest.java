6f1d98685aeb2c0e3f9cb93ed408f7e3e2fbbc80
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2019-12-18 19:04:10 -0500

    First Checkin.  Early tests.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RampTest.java
@@ -0,0 +1,116 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import static java.lang.System.*;
+
+
+@TeleOp(name = "Ramp Test",  group="Iterative Opmode")
+//  @Disabled
+public class RampTest extends LinearOpMode {
+
+    static final double INCREMENT   = 0.01;     // amount to ramp motor each CYCLE_MS cycle
+    static final int    CYCLE_MS    =   50;     // period of each cycle
+    static final double MAX_FWD     =  1.0;     // Maximum FWD power applied to motor
+    static final double MAX_REV     = -1.0;     // Maximum REV power applied to motor
+
+    // Define class members
+    DcMotor motor;
+    SpeedMotor speedMotor;
+    double  power   = 0;
+    boolean rampUp  = true;
+
+
+    @Override
+    public void runOpMode() {
+
+        // Connect to motor (Assume standard left wheel)
+        // Change the text in quotes to match any motor name on your robot.
+        motor = hardwareMap.get(DcMotor.class, "left_drive");
+        speedMotor = new SpeedMotor(motor);
+
+        // Wait for the start button
+        telemetry.addData(">", "Press Start to run Motors." );
+        telemetry.update();
+        waitForStart();
+
+        // Ramp motor speeds till stop pressed.
+        while(opModeIsActive()) {
+
+            // Ramp the motors, according to the rampUp variable.
+            if (rampUp) {
+                // Keep stepping up until we hit the max value.
+                power += INCREMENT ;
+                if (power >= MAX_FWD ) {
+                    power = MAX_FWD;
+                    rampUp = !rampUp;   // Switch ramp direction
+                }
+            }
+            else {
+                // Keep stepping down until we hit the min value.
+                power -= INCREMENT ;
+                if (power <= MAX_REV ) {
+                    power = MAX_REV;
+                    rampUp = !rampUp;  // Switch ramp direction
+                }
+            }
+
+            // Display the current value
+
+            telemetry.addData("Motor Power", "%5.2f", power);
+            telemetry.addData(">", "Press Stop to end test." );
+
+            // Set the motor to the new power and pause;
+            motor.setPower(power);
+            double end = currentTimeMillis()+CYCLE_MS;
+            while (currentTimeMillis() < end) {
+                speedMotor.autoAdjust();
+            }
+            telemetry.addData("Speed", speedMotor.getCurrentSpeed() );
+
+            telemetry.addData("Updates Per Second", speedMotor.getUpdatesPerSecond() );
+            speedMotor.getUpdatesPerSecond();
+            telemetry.update();
+
+            //sleep(CYCLE_MS);
+        }
+
+        // Turn off motor and signal done;
+        motor.setPower(0);
+        telemetry.addData(">", "Done");
+        telemetry.update();
+
+    }
+}
