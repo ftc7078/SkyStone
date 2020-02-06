8b76572c89181e38d59919b591cb20fb86f18829
ftc7078 <59035211+ftc7078@users.noreply.github.com>
2020-02-06 13:02:20 -0500

    Update adbconnect.bat

--- onbot-utility/adbconnect.bat
+++ onbot-utility/adbconnect.bat
@@ -1,7 +1,7 @@
 @cd platform-tools
 adb devices
 netsh wlan connect name=7078-RC interface="Wi-Fi 2"
-adb connect 192.168.43.1
+adb connect 192.168.43.1:5555
 @REM adb connect 192.168.49.1
 @cd ..
 
